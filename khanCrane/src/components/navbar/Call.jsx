e049a3fd81eb524fbe546709a4882fdf70479a3e
karma2912 <rajakyash23@gmail.com>
2025-02-04 13:18:06 +0530

    commit

--- khanCrane/src/components/navbar/Call.jsx
+++ khanCrane/src/components/navbar/Call.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
 const Call = () => {
-  const phoneNumber = "9561829120"; // Replace with your phone number
+  const phoneNumbers = ["9821661352", "9967519974", "8369594756"];
 
   return (
-    <a href={`tel:${phoneNumber}`} className="fixed bottom-5 right-5 bg-yellow-500 p-3 z-50 rounded-full shadow-lg">
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/8/83/Circle-icons-phone.svg" // Replace with your contact icon
-        alt="Call Us"
-        style={{ width: "50px", height: "50px", cursor: "pointer" }}
-      />
-    </a>
+    <div className="fixed bottom-5 right-5 flex flex-col space-y-2 z-50">
+      {phoneNumbers.map((number, index) => (
+        <a
+          key={index}
+          href={`tel:${number}`}
+          className="bg-yellow-500 p-3 z-50 rounded-full shadow-lg text-white font-bold"
+        >
+          Call {number}
+        </a>
+      ))}
+    </div>
   );
 };
 
